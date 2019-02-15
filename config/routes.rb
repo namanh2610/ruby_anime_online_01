5f328ebf885baef5d9adb4e3304e5675d7ed5737
Chu Thành Hưng <thanhhungchu95@gmail.com>
2019-02-15 11:42:44 +0700

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root to: "static_pages#home"
+    root "static_pages#home"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :movies
+    resources :user
+    namespace :admin do
+      get "/show", to: "admins#show"
+      resources :admins
+    end
   end
 end
